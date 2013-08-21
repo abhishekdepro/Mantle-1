28fbe490a5e221bc31f0c781a2ff7d1fd329baf9
Abhishek De <abhishekde@hotmail.com>
2013-08-21 17:02:43 +0530

    Fixed issues with sd
    Changed Bing condition and removed ch10. will bring it up using F-C
    converter.

--- sdweather.py
+++ sdweather.py
@@ -32,7 +32,7 @@ htmlbing=sourcebing.read()
 
 # search for pattern using regular expressions for bing (.+?)
 tempbing='<div class="w_cur_tb">(.+?)&#176;</div>'
-weatherbing='>(.+?)</div><div>&#176;C&nbsp;'
+weatherbing='<div class="w_con_txtn" title="(.+?)">'
 patternbing=re.compile(tempbing)
 pattern1bing=re.compile(weatherbing)
 
@@ -52,27 +52,27 @@ patternask=re.compile(tempask)
 weather_tempask=re.findall(patternask,htmlask)
 
 
-urlch10="http://www.weatherchannel10.com/in/%s" % city
-str10='.html'
-urlch10=urlch10+str10
-sourcech10=urllib2.urlopen(urlch10)
-htmlch10=sourcech10.read()
+#urlch10="http://www.weatherchannel10.com/in/%s" % city
+#str10='.html'
+#urlch10=urlch10+str10
+#sourcech10=urllib2.urlopen(urlch10)
+#htmlch10=sourcech10.read()
 # search for pattern using regular expressions for weatherchannel10 (.+?)
-tempch10='/>(.+?)°</div></td><td colspan="2" class="weather">'
+#tempch10='/>(.+?)°</div></td><td colspan="2" class="weather">'
 
-patternch10=re.compile(tempch10)
+#patternch10=re.compile(tempch10)
 
 # match pattern with htmltext for weatherchannel10
-weather_tempch10=re.findall(patternch10,htmlch10)
+#weather_tempch10=re.findall(patternch10,htmlch10)
 
- urlwf="http://www.weather-forecast.com/locations/%s" % city
- strwf='/metars/latest'
- urlwf=urlwf+strwf
- sourcewf=urllib2.urlopen(urlwf)
- htmlwf=sourcewf.read()
- # search for pattern using regular expressions for weather-forecast (.+?)
- tempwf='#FF0000;"><span class="temp">(.+?)</span></td>'
- patternwf=re.compile(tempwf)
+urlwf="http://www.weather-forecast.com/locations/%s" % city
+strwf='/metars/latest'
+urlwf=urlwf+strwf
+sourcewf=urllib2.urlopen(urlwf)
+htmlwf=sourcewf.read()
+# search for pattern using regular expressions for weather-forecast (.+?)
+tempwf='#FF0000;"><span class="temp">(.+?)</span></td>'
+patternwf=re.compile(tempwf)
 
 
 # match pattern with htmltext for weather-forecast
@@ -81,26 +81,26 @@ weather_tempwf=re.findall(patternwf,htmlwf)
 a=int(weather_temp[0])
 b=int(weather_tempbing[0])
 c=int(weather_tempask[0])
-d=int(weather_tempch10[0])
+#d=int(weather_tempch10[0])
 e=int(weather_tempwf[0])
-mean=a+b+c+d+e
+mean=a+b+c+e
 
-mean=mean/5
+mean=mean/4
 
 a=a-mean
 b=b-mean
 c=c-mean
-d=d-mean
+#d=d-mean
 e=e-mean
 
 a=math.pow(a,2)
 b=math.pow(b,2)
 c=math.pow(c,2)
-d=math.pow(d,2)
+#d=math.pow(d,2)
 e=math.pow(e,2)
 
-sd=a+b+c+d+e
-sd=sd/5
+sd=a+b+c+e
+sd=sd/4
 sd=math.sqrt(sd)
 
 
@@ -120,7 +120,7 @@ print "Weather Condition is (bing) : ",weather_conditionbing[0]
 
 print "Temperature is (ask): ",weather_tempask[0]," degree celcius"
 
-print "Temperature is (ch10): ",weather_tempch10[0]," degree celcius"
+#print "Temperature is (ch10): ",weather_tempch10[0]," degree celcius"
 
 print "Temperature is (wf): ",weather_tempwf[0]," degree celcius"
 
